991053ecb4164a7639aab874b63809b373e4cdd1
Nasr eddine SARMOUME <140182661+Nasreddine88@users.noreply.github.com>
2023-08-29 04:19:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict'
+// Night Mode funtionality
+    const styleSheet = document.getElementById("stylesheet");
+    const lightButton = document.querySelector(".lightButton");
+    const darkButton = document.querySelector(".darkButton");
+    darkButton.addEventListener("click", function() {
+        styleSheet.href = "css/color-2.css"
+    });
+    lightButton.addEventListener("click", function() {
+        styleSheet.href = "css/color-1.css"
+    });
+    // Menu functionality
+    const menuController = document.querySelector(".menu-controller");
+    const openMenu = document.querySelector(".menu");
+    const menuIcon = document.querySelector(".menu-icon");
+
+    menuController.addEventListener("click", function() {
+        menuController.classList.toggle("menu-controller-btn");
+        openMenu.classList.toggle("open");
+        menuIcon.classList.toggle("fa-xmark");
+    });
+
+    window.addEventListener("scroll", function() {
+        menuController.classList.remove("menu-controller-btn");
+        openMenu.classList.remove("open");
+    });
+    const nav = document.querySelector(".nav"),
+          navList = nav.querySelectorAll("li"),
+          totalNavList = navList.length;
+    for (let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for (let j=0; j<totalNavList; j++)
+            {
+                navList[j].querySelector("a").classList.remove("active-link");
+                console.log(a);
+            }
+            this.classList.add("active-link");
+            menuController.classList.remove("menu-controller-btn");
+            openMenu.classList.remove("open");
+        });
+    }
